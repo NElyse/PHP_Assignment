685701330aa8dc789ca661f952ec7ab0342fd3d0
NElyse <105349034+NElyse@users.noreply.github.com>
2022-05-26 16:33:20 -0700

    Update pass_rest.php

--- pass_rest.php
+++ pass_rest.php
@@ -7,7 +7,7 @@
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
             <link rel="stylesheet" href="boostrap\css\bootstrap.min.css">
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/boostrap\js\bootstrap.bundle.min.js"></script>
-    <title>Dashboard</title>
-</head>
+    <title>Reset password</title>
+
 <body>
    
