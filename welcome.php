b454624ce50c7004eea60c204c2b4373a64c49d0
NElyse <elinsengimana@gmail.com>
2022-05-27 01:04:49 -0700

    end of we

--- welcome.php
+++ welcome.php
@@ -40,10 +40,12 @@
 
         
 
-        <div class="row bg-info" style="width:200vh">
-            <div class="col-md-8 ">
+        <div class="body-cont d-flex">
+            < class="col-md-8 ">
                 <div class="col">
-                    cover image
+                <div class="col-sm-8 col-md-12">
+                    <img class="col-sm-12 col-md-12" src="caverpage.jpg" style="height: 40vh; width:auto" alt="background-large">
+                </div>
             
 
                 <form method="POST" action="#.php" id="wraper">        
@@ -63,17 +65,74 @@
                 <b> headline</b>
                 
                 
-            </div>
+            
 
-                   
-
-                
                  </div>
+
+                 <div class="body-cont d-flex">
+
+
+                        <div class="left" style="padding-left: 8vh; width: 140vh;">
+
+                            <div class="">
+                                <img class="col-sm-12 col-md-12" src="coverpage.jpg" style="height:50vh;" alt="background-large">
+                            </div>
+
+                            <div class="position-absolute d-flex" style="padding-left: 4vh; top: 90px;"> 
+                                <img class="rounded-circle z-depth-2" style="width: 145px;" src="profile.jpg" alt="profile-pc">
+                                <h6 class="col-sm-8 col-md-12 text-white position-relative" style="top: 70px; font-size: 18px; padding-left: 3vh;">NSENGIMANA Elyse</h6>
+                            </div>
+
+                            <hr class="col-sm-8 col-md-12 mt-4 mb-3">
+
+                            <div class="d-flex" style="padding-left: 8vh;">
+                                <h6><b>Headline</b></h6>
+                                <a href="#" class="position-absolute" style="left: 176px; top: 265px;"><i class="fa fa-pencil mx-2 text-black" aria-hidden="true"></i></a>
+                            </div>
+
+                            <h6 style="font-size: 14px;">Software Engineer | Data Analyst</h6>
+
+                        
+
+                            <form action="" method="post">
+                                <div class="col-sm-8 col-md-12 d-flex bg-info">
+                                    <i class="fa fa-key mx-2 mt-2" aria-hidden="true"></i>
+                                    <h6 class="text-white mt-1"><b>Change Password</b></h6>
+                                </div>
+
+                                <div class="mt-5 mx-2">
+                                    <div class="d-flex">
+                                        <h6 class="mx-4 mt-2">Current Password</h6>
+                                        <div class="position-relative">
+                                            <input class="ps-5 form-control" placeholder="Password" type="password" name="" id="">
+                                            <i class="fa fa-key text-info position-absolute" style="top: 13px; left: 15px;" aria-hidden="true"></i>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex mt-3">
+                                        <h6 class="mx-5 mt-2">New Password</h6>
+                                        <div class="position-relative" style="left: -31px;">
+                                            <input class="ps-5 form-control" placeholder="Password" type="password" name="" id="">
+                                            <i class="fa fa-key text-info position-absolute" style="top: 13px; left: 15px;" aria-hidden="true"></i>
+                                            <i class="fa fa-eye text-info position-absolute" style="top: 13px; right: 16px;" aria-hidden="true"></i>
+                                        </div>
+                                    </div>
+
+
+                                    <div class="d-flex">
+                                        <input class="form-check-input position-absolute" style="left:auto; top: 545px;"  type="checkbox" name="" id="">
+                                        <p class="position-absolute" style="font-size: 11px; top: auto; left: 308px;"><b>remind me to change password for each 3 months</b></p>
+                                    </div>
+
+                                    <button class="my-5 btn btn-info text-white position-absolute" style="left: 388px;" type="submit">Set New Password</button>
+                                    <div class="my-5 position-absolute" style="top: 600px;"><h5>.</h5></div>
+                                </div>
+                            </form>
+                        </div>
 
                 <div class="col">
                     
                 </div>
-            </div>
+                
             <div class="col-md-4">
                 
                 <div class="col text-center border border-top border-info bg-white" style="width: 65vh;" ><h6 class="text-info bg-white"  > Account Settings</h6></div>
